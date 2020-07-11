9129b7a9797d90c63f37de29404881e5cb913dab
jason-aultman <jason.aultman@gmail.com>
2020-07-10 21:13:27 -0400

    initial commit

---
+++ GCLab2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace GCLab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool again = false;
+            do
+            {
+                System.Console.Clear();  //Went outside scope of project for cleanliness
+                Console.WriteLine("Welcome to Jasons' Room Detail Generator");
+                Console.WriteLine();
+                Console.Write("Enter length: ");
+                var lengthPassed= double.TryParse(Console.ReadLine(), out double lengthAsDouble);
+                Console.Write("Enter width: ");
+                var widthPassed = double.TryParse(Console.ReadLine(), out double widthAsDouble);
+                Console.WriteLine();
+                if (!lengthPassed || !widthPassed) //input validation to prevent program crash on bad input --TODO add another loop within system to reenter input.
+                {
+                    Console.WriteLine("There was a problem with the input. Please try again.  Exiting...");
+                    return;
+                }
+                Console.WriteLine("Area: " + (lengthAsDouble*widthAsDouble));
+                Console.WriteLine("Perimeter: " + ((2 * lengthAsDouble) + (2 * widthAsDouble)));
+                Console.WriteLine();
+                Console.Write("Continue? (y/n) ");
+                var doMore = Console.ReadLine();
+                if (doMore == "y" || doMore == "Y")
+                {
+                    again = true;
+                }
+                else again = false;
+            } while (again);
+        }
+    }
+}
